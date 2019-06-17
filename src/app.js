8481982df2edf7c1a4ebd3dbbc8b1a7d33f345d1
Qiugu He <kaochi545121151@gmail.com>
2019-06-16 22:15:53 -0500

    Using npm hbs and handlerBar
    So the Templet engine, which is npm handlerbar and hbs module for render dynamic web page

--- src/app.js
+++ src/app.js
@@ -2,12 +2,32 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
-
 //static take the folder, this will find all the html file 
 const publicDirPath =path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs') // set up handlebar for templets
 app.use(express.static(publicDirPath)) 
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title:'About me',
+        name: 'QQQ'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title:'Weather App',
+        name: 'HHHe'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {  //render to help page
+        helpText:'some help text',
+        name: 'HHHe'
+    })
+})
 
 app.get('/weather', (req,res)=>{
     res.send({
