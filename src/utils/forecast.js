1f56e98be7d7222accab8b2b935b83ccc6b6e0c9
Qiugu He <kaochi545121151@gmail.com>
2020-06-19 21:01:32 -0500

    Updated UI

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,7 @@ const request = require('request')
 const forecast = (lat, longt, callback)=>{
     const url = 'https://api.darksky.net/forecast/6edfa9fb3e21032224ea83f69ad86ad0/'+lat +',' +longt
 
+
     request({url, json: true}, (error, {body})=>{
         if(error){
             callback('Unable to connect to weather services!', undefined)// this function will send back to caller 'geocode'
@@ -11,7 +12,7 @@ const forecast = (lat, longt, callback)=>{
         }else{
             callback(undefined, body.daily.data[0].summary + 
             ' It is currently ' + body.currently.temperature + 
-            ' degress out. This high today is' + body.daily.data[0].temperatureHigh +' with the low of '+body.daily.data[0].temperatureLow +'. There is a ' + body.currently.precipProbability +
+            ' degress out. This high today is ' + body.daily.data[0].temperatureHigh +' with the low of '+body.daily.data[0].temperatureLow +'. There is a ' + body.currently.precipProbability +
             ' % chance of rain. ') // when call back invoked, undefined is returned for error parameter, and another parameter is for data parameter
         }
     })
