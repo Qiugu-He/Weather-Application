ce200cd21c7ce50181667d1d3930822729986892
Qiugu He <kaochi545121151@gmail.com>
2020-06-19 21:22:43 -0500

    Updated UI

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title:'About me',
+        title:'About Application',
         name: 'Qiu'
     })
 })
